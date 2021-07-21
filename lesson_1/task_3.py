a8853a49e95e551ba39ae0b80deee7588e9972b6
Anna Istomina <aninnkaista@gmail.com>
2021-07-21 15:06:44 +0200

    :bookmark: all tasks for lesson 2 added

--- lesson_1/task_3.py
+++ lesson_1/task_3.py
@@ -8,8 +8,14 @@ def get_line_params(point_1: tuple, point_2: tuple):
     y2 = (y1 - b)/x1 * x2 + b
     y2 = y1 / x1 * x2 - b*x2 / x1 + b
     y2 - y1/x1 * x2 = b(1 - x2/x1)"""
+
     x1, y1 = point_1
     x2, y2 = point_2
+
+    if x1 == 0:
+        b = y1
+        k = (y2 - y1)
+    ## to FINISH
     b = (y2 - y1 / x1 * x2) / (1 - x2 / x1)
     k = (y1 - b) / x1
     return round(k, 2), round(b, 2)
